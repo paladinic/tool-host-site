dedbb2475fc1b9a783685b27a5876bb732c238ce
Claudio Paladini <clode.paladini@gmail.com>
2021-10-01 08:32:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    // document.getElementById("logo-img").style.width = "0";
+  } else {
+      // document.getElementById("logo-img").style.width = "4%";
+  }
+}
