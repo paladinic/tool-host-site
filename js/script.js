c976cf2aef821ff1c03a495db47bb901944213d6
Claudio <clode.paladini@gmail.com>
2022-01-30 19:55:13 +0000

    restyle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    // document.getElementById("logo-img").style.width = "0";
+      document.querySelector('#logo img').style.width = '120px';
+      document.querySelector('#logo img').style.height = '60px';
+      document.querySelector('#logo img').style.padding = '10px';
+      document.querySelector('.header a').style.fontSize = '0px';
   } else {
-      // document.getElementById("logo-img").style.width = "4%";
+    document.querySelector('#logo img').style.width = '200px';
+    document.querySelector('#logo img').style.height = '100px';
+    document.querySelector('#logo img').style.padding = '5px';
+    document.querySelector('.header a').style.fontSize = '20px';
   }
 }
